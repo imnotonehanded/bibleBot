7820b93e8adb15c8a0ab84b9ba9cd541972a1ea3
imnotonehanded <59941757+imnotonehanded@users.noreply.github.com>
2021-02-15 14:59:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import discord
 from discord.ext import commands
 bot = commands.Bot(command_prefix='$')
 bot.remove_command("help")
-
+token = input("Enter Bot Token: ")
 @bot.command()
 async def twentyonesix(ctx):
     await ctx.send("And he said unto me, It is done. I am Alpha and Omega, the beginning and the end. I will give unto him that is athirst of the fountain of the water of life freely.")
     print("sent")
-bot.run("ODEwOTYyMTU3NDM2NjY1ODg3.YCrRXQ.wqOCR8o0rzj0WA46nqD2KrCOApM")
+bot.run()
